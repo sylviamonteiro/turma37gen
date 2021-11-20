35c8164faf9ac31503587e0d4c23258f4d5012db
sylviamonteiro <sylviacmmoraes@gmail.com>
2021-11-20 13:02:06 -0300

    UPGRADE POM AND BASIC SECURITY CONFIG

--- java/blogPessoal/src/main/java/com/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ java/blogPessoal/src/main/java/com/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -20,6 +20,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+		auth.inMemoryAuthentication().withUser("boaz").password(passwordEncoder().encode("boaz"))
+        .authorities("ROLE_ADMIN");
 	}
 	
 	@Bean
