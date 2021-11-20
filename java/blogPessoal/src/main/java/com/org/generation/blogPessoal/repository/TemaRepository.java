45dee981865096849496a965e0b447477c3d19f7
sylviamonteiro <sylviacmmoraes@gmail.com>
2021-11-20 12:46:57 -0300

    @sylviamonteiro [Task: Fixed #2]

--- java/blogPessoal/src/main/java/com/org/generation/blogPessoal/repository/TemaRepository.java
+++ java/blogPessoal/src/main/java/com/org/generation/blogPessoal/repository/TemaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.org.generation.blogPessoal.model.TemaModel;
+
 @Repository
 public interface TemaRepository extends JpaRepository<TemaModel, Long> {
 
