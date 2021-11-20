3a1f0f10db9d7d19af1baefd1db4612b8e966c2b
sylviamonteiro <sylviacmmoraes@gmail.com>
2021-11-20 10:58:23 -0300

    @sylviamonteiro [Task: Fixed Issue #1]

--- java/blogPessoal/src/main/java/com/org/generation/blogPessoal/model/PostagemModel.java
+++ java/blogPessoal/src/main/java/com/org/generation/blogPessoal/model/PostagemModel.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -38,6 +39,12 @@ public class PostagemModel {
 	@ManyToOne
 	@JsonIgnoreProperties("postagens")
 	private TemaModel temaPostagem;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_id")
+	@JsonIgnoreProperties({"minhasPostagens"})
+	private Usuario criador;
+
 
 	public TemaModel getTemaPostagem() {
 		return temaPostagem;
@@ -79,4 +86,12 @@ public class PostagemModel {
 		this.data = data;
 	}
 
+	public Usuario getCriador() {
+		return criador;
+	}
+
+	public void setCriador(Usuario criador) {
+		this.criador = criador;
+	}
+	
 }
--- java/blogPessoal/src/main/java/com/org/generation/blogPessoal/model/Usuario.java
+++ java/blogPessoal/src/main/java/com/org/generation/blogPessoal/model/Usuario.java
@@ -1,12 +1,22 @@
 package com.org.generation.blogPessoal.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.annotations.ApiModelProperty;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -20,13 +30,21 @@ public class Usuario {
 	@Size(min = 2, max = 100)
 	private String nome;
 
-	@NotNull
+	@ApiModelProperty (example = "email@email.com.br")
+	@Email (message = "O atributo Usuário deve ser um email válido!")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
 	@Size(min = 5, max = 100)
 	private String usuario;
 
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String senha;
+	
+	private String foto;
+	
+	@OneToMany(mappedBy = "criador", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"criador"})
+	private List<PostagemModel> minhasPostagens = new ArrayList<>();
 
 	public long getId() {
 		return id;
@@ -58,6 +76,21 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public List<PostagemModel> getMinhasPostagens() {
+		return minhasPostagens;
+	}
+
+	public void setMinhasPostagens(List<PostagemModel> minhasPostagens) {
+		this.minhasPostagens = minhasPostagens;
 	}
 
 }
